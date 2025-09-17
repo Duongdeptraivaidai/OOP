d50208e61ff2f18e449ba4fc6967c22d022e24dc
Nguyễn Quốc Đương <dathuynh594@gmail.com>
2025-09-17 16:55:12 +0700

    Update main.cpp

--- HinhChuNhatoop/XepLoaiHSoop/main.cpp
+++ HinhChuNhatoop/XepLoaiHSoop/main.cpp
@@ -1,12 +1,21 @@
-#include"XepLoaiHS.h"
+#include "HocSinh.h"
+using namespace std;
 
 int main() {
-	XepLoaiHS xl;
-	xl.nhap();
-	xl.xuat();
-	float dtb = xl.diemTB();
-	string xeploai = xl.xepLoai();
-	cout << "Diem trung binh la: " << dtb << endl;
-	cout << "Xep loai: " << xeploai << endl;
-	return 0;
+   
+    XepLoaiHS hs1;
+    hs1.nhap();
+    hs1.xuat();
+    cout << "Diem trung binh: " << hs1.diemTB() << endl;
+    cout << "Xep loai: " << hs1.xepLoai() << endl;
+
+    cout << "--------------------" << endl;
+
+  
+    XepLoaiHS hs2("Nguyen Van A", 8.0, 7.5);
+    hs2.xuat();
+    cout << "Diem trung binh: " << hs2.diemTB() << endl;
+    cout << "Xep loai: " << hs2.xepLoai() << endl;
+
+    return 0;
 }
