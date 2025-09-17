a155fee2ab86f35230eeee7d950e0e6cac7cc0f6
Nguyễn Quốc Đương <dathuynh594@gmail.com>
2025-09-17 16:55:54 +0700

    Update XepLoaiHS.h

--- HinhChuNhatoop/XepLoaiHSoop/XepLoaiHS.h
+++ HinhChuNhatoop/XepLoaiHSoop/XepLoaiHS.h
@@ -1,50 +1,29 @@
-#pragma once
-#include<iostream>
-#include<string>
-using namespace std;
-class XepLoaiHS
-{
-private:
-	string HoTen;
-	float dToan;
-	float dVan;
-public:
-	XepLoaiHS() {
-		this->HoTen = "";
-		this->dToan = 0;
-		this->dVan = 0;
-	}
-	XepLoaiHS(string ht, float dt, float dv) {
-		this->HoTen = ht;
-		this->dToan = dt;
-		this->dVan = dv;
-	}
-	~XepLoaiHS() {
-		cout << "Huy hoc sinh" << endl;
-	}
+#include"PhanSo.h"
 
-	void nhap() {
-		cout << "Nhap HoTen: ";
+int main() {
 
-		getline(cin, this->HoTen);
-		cout << "Nhap diem Toan: "; cin >> this->dToan;
-		cout << "Nhap diem Van: "; cin >> this->dVan;
-	}
 
-	float diemTB() {
-		return (this->dToan + this->dVan) / 2;
-	}
-	void xuat() {
-		cout << "Ho ten HS: " << this->HoTen << endl;
-		cout << "diem toan la: " << this->dToan << endl;
-		cout << "diem van la: " << this->dVan << endl;
-	}
+    PhanSo ps1, ps2;
+    cout << "Nhap phan so thu nhat: ";
+    ps1.nhap();
+    cout << "Nhap phan so thu hai: ";
+    ps2.nhap();
 
-	string xepLoai() {
-		if (diemTB() >= 7.5)return "Loai gioi";
-		else if (diemTB() >= 5)return "Loai kha";
-		else if (diemTB() >= 3.5)return "Loai kem";
-		else return "Loai yeu";
-	}
-};
-	
+    PhanSo psTong = ps1.cong(ps2);
+    cout << "Tong hai phan so: ";
+    psTong.xuat();
+
+    PhanSo psHieu = ps1.tru(ps2);
+    cout << "Hieu hai phan so: ";
+    psHieu.xuat();
+
+    PhanSo psTich = ps1.nhan(ps2);
+    cout << "Tich hai phan so: ";
+    psTich.xuat();
+
+    PhanSo psThuong = ps1.chia(ps2);
+    cout << "Thuong hai phan so: ";
+    psThuong.xuat();
+
+    return 0;
+}
