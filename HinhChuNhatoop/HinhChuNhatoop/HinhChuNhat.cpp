4bbec705f08feaeb24bca443e0f1203e5a4fa181
Nguyễn Quốc Đương <dathuynh594@gmail.com>
2025-09-17 16:53:32 +0700

    Update HinhChuNhat.cpp

--- HinhChuNhatoop/HinhChuNhatoop/HinhChuNhat.cpp
+++ HinhChuNhatoop/HinhChuNhatoop/HinhChuNhat.cpp
@@ -1,17 +1,1 @@
-#include "HinhChuNhat.h"
 
-
-void HinhChuNhat::input(){
-	cout << "Nhap chieu dai:"; cin >> this->Long;
-	cout << "Nhap chieu rong: "; cin >> this->wide;
-}
-void HinhChuNhat::output() {
-	cout << "Chieu dai la: " << this->Long << endl;
-	cout << "Chieu rong la: " << this->wide << endl;
-}
-float HinhChuNhat::dientich() {
-	return this->Long * this->wide;
-}
-float HinhChuNhat ::chuvi() {
-	return (this->Long + this->wide) * 2;
-}
