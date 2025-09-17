c6a809bf5b661cfc691b206f944ba8c167bab132
Nguyễn Quốc Đương <dathuynh594@gmail.com>
2025-09-17 16:54:22 +0700

    Update Main.cpp

--- HinhChuNhatoop/HaiToaDoKhongGian/Main.cpp
+++ HinhChuNhatoop/HaiToaDoKhongGian/Main.cpp
@@ -1,8 +1,14 @@
-#include"HaiToaDoKhongGian.h"
+#include"HaiToaDoKhongGianXY.h"
 
 int main() {
-    HaiToaDoKhongGian toaDo;
-    toaDo.nhap();
-    toaDo.xuat();
+    HaiToaDoKhongGian toaDo1;
+    HaiToaDoKhongGian toaDo2(3.2, 4.2,3.3,4.7);
+
+
+    toaDo1.nhap();
+    toaDo1.xuat();
+    cout << endl;
+    toaDo2.xuat();
 
     return 0;
+}
