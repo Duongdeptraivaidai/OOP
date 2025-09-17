7bb3318d497a2a4a2abbd6c0f8d717acdb078844
Nguyễn Quốc Đương <dathuynh594@gmail.com>
2025-09-17 16:50:42 +0700

    Update Main.cpp

--- HinhChuNhatoop/BaDinhTamGiac/Main.cpp
+++ HinhChuNhatoop/BaDinhTamGiac/Main.cpp
@@ -1,11 +1,12 @@
-#include "BaDinhTamGiac.h"
+#include "TamGiac.h"
 
 int main() {
-	BaDinhTamGiac tg;
-	tg.nhap();
-	tg.xuat();
-	float cv = tg.chuViTamGiac();
-	float dt = tg.dienTichTamGiac();
+	BaDinhTamGiac tg1;
+	BaDinhTamGiac tg2(7, 8, 9);
+	tg1.nhap();
+	tg1.xuat();
+	float cv = tg1.chuViTamGiac();
+	float dt = tg1.dienTichTamGiac();
 	cout << "Dien tich TamGiac la: " << dt << endl;
 	cout << "Chuvi TamGiac la: " << cv << endl;
 	return 0;
