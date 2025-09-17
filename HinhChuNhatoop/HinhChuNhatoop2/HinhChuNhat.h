0bb626372dcf815eb34c94129fe2e6abd3731be4
Nguyễn Quốc Đương <dathuynh594@gmail.com>
2025-09-17 16:57:30 +0700

    Update HinhChuNhat.h

--- HinhChuNhatoop/HinhChuNhatoop2/HinhChuNhat.h
+++ HinhChuNhatoop/HinhChuNhatoop2/HinhChuNhat.h
@@ -4,24 +4,39 @@ using namespace std;
 class HinhChuNhat
 {
 private:
-	float chieuDai;
-	float chieuRong;
+	float Long;
+	float wide;
 public:
+	HinhChuNhat() {
+		this->Long = 0;
+		this->wide = 0;
+	}
+
+	HinhChuNhat(float L, float w) {
+		this->Long = L;
+		this->wide = w;
+		cout << "Khoi tao HinhChuNhat voi chieu dai: " << L << " va chieu rong: " << w << endl;
+	}
+
+	~HinhChuNhat() {
+		cout << "Hinh chu nhat bi huy" << endl;
+	}
+
 	void nhap() {
-		cout << "Nhat chieu dai: ";
-		cin >> this->chieuDai;
-		cout << "Nhat chieu rong: ";
-		cin >> this->chieuRong;
+		cout << "Nhap chieu dai:"; cin >> this->Long;
+		cout << "Nhap chieu rong: "; cin >> this->wide;
+
 	}
 	void xuat() {
-		cout << "Chieu dai la: " << this->chieuDai << endl;
-		cout << "Chieu rong la: " << this->chieuRong << endl;
+		cout << "Chieu dai la: " << this->Long << endl;
+		cout << "Chieu rong la: " << this->wide << endl;
 	}
-	float dientich() {
-		return this->chieuDai * this->chieuRong;
+
+	float dienTich() {
+		return this->Long * this->wide;
 	}
-	float chuvi() {
-		return (this->chieuDai + this->chieuRong) * 2;
+
+	float chuVi() {
+		return (this->Long + this->wide) * 2;
 	}
 };
-
