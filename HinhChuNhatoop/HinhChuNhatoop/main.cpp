b106ba0da3ececa3c991de6ba50a7bf6cdfd2514
Nguyễn Quốc Đương <dathuynh594@gmail.com>
2025-09-17 16:53:49 +0700

    Update main.cpp

--- HinhChuNhatoop/HinhChuNhatoop/main.cpp
+++ HinhChuNhatoop/HinhChuNhatoop/main.cpp
@@ -1,11 +1,14 @@
 #include"HinhChuNhat.h"
 
-int main(){
-	HinhChuNhat hcn;
-	hcn.input();
-	hcn.output();
-	float dt = hcn.dientich();
-	float cv = hcn.chuvi();
+int main() {
+	HinhChuNhat hcn1;
+	HinhChuNhat hcn2(10.5, 5.3);
+
+	hcn1.nhap();
+	hcn1.xuat();
+	float dt = hcn1.dienTich();
+	float cv = hcn1.chuVi();
 	cout << "Dien tich la: " << dt << endl;
 	cout << "Chu vi la: " << cv << endl;
+	return 0;
 }
