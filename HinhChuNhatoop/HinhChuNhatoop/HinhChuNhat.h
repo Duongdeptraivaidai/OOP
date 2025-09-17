20fe142230ba001b1cacf7ea59fe31ec904156b3
Nguyễn Quốc Đương <dathuynh594@gmail.com>
2025-09-17 16:51:29 +0700

    Update HinhChuNhat.h

--- HinhChuNhatoop/HinhChuNhatoop/HinhChuNhat.h
+++ HinhChuNhatoop/HinhChuNhatoop/HinhChuNhat.h
@@ -7,9 +7,36 @@ private:
 	float Long;
 	float wide;
 public:
-	void input();
-	void output();
-	float dientich();
-	float chuvi();
-};
+	HinhChuNhat() {
+		this->Long = 0;
+		this->wide = 0;
+	}
 
+	HinhChuNhat(float L, float w) {
+		this->Long = L;
+		this->wide = w;
+		cout << "Khoi tao HinhChuNhat voi chieu dai: " << L << " va chieu rong: " << w << endl;
+	}
+
+	~HinhChuNhat() {
+		cout << "Hinh chu nhat bi huy" << endl;
+	}
+
+	void nhap() {
+		cout << "Nhap chieu dai:"; cin >> this->Long;
+		cout << "Nhap chieu rong: "; cin >> this->wide;
+
+	}
+	void xuat() {
+		cout << "Chieu dai la: " << this->Long << endl;
+		cout << "Chieu rong la: " << this->wide << endl;
+	}
+
+	float dienTich() {
+		return this->Long * this->wide;
+	}
+
+	float chuVi() {
+		return (this->Long + this->wide) * 2;
+	}
+};
